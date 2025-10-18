4d2b541c999475d529e784c49920bfa6c16cf4c4
João Vitor <joao789roye@gmail.com>
2025-10-18 14:33:26 -0300

    feat(build): criando setup do projeto

---
+++ dist/ui/renderer.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Renderer process iniciado!");
+document.body.innerHTML = "<h1>Olá, Electron + TypeScript! 🚀</h1>";
---
+++ src/ui/renderer.ts
@@ -0,0 +1,2 @@
+console.log("Renderer process iniciado!");
+document.body.innerHTML = "<h1>Olá, Electron + TypeScript! 🚀</h1>";
