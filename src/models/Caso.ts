4f9513b57050dce643da97ee4a7a31f68bfd6e69
João Vitor <joao789roye@gmail.com>
2025-10-20 23:40:17 -0300

    feat(anexos): criando model de anexos

--- src/models/Caso.ts
+++ src/models/Caso.ts
@@ -1,5 +1,8 @@
+import { Anexo } from "./Anexo";
+
 export class Caso {
     private protocoloCaso?: number;
+    private anexos: Anexo[] = [];
     
     constructor(protocoloCaso: number) {
         this.protocoloCaso = protocoloCaso;
@@ -8,5 +11,13 @@ export class Caso {
     // Getters
     public getProtocoloCaso(): number | undefined {
         return this.protocoloCaso;
+    }
+
+    public getAnexos(): Anexo[] {
+        return this.anexos;
+    }
+
+    public getAnexoById(idAnexo: number): Anexo | undefined{
+        return this.anexos.find(anexos => anexos.getidAnexo() === idAnexo);
     }
 }
