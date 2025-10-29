0d72622ea3cecda274ee7ad4ee8b45e3b779d39f
Clidenor <clidenorlopesmartinsfilho@gmail.com>
2025-10-29 09:26:28 -0300

    refactor(AssistenteSocial): Refatora classe para herdar de User

--- src/models/AssistenteSocial.ts
+++ src/models/AssistenteSocial.ts
@@ -1,27 +1,8 @@
-export class AssistenteSocial {
-    private nome: string;
-    private emailInstitucional: string;
+import { User } from "./User";
+
+export class AssistenteSocial extends User {
 
     constructor(nome: string, emailInstitucional: string) {
-        this.nome = nome;
-        this.emailInstitucional = emailInstitucional;
-    }
-
-    // Getters
-    public getNome(): string {
-        return this.nome;
-    }
-
-    public getEmailInstitucional(): string {
-        return this.emailInstitucional;
-    }
-
-    // Setters
-    public setNome(nome: string): void {
-        this.nome = nome;
-    }
-
-    public setEmailInstitucional(emailInstitucional: string): void {
-        this.emailInstitucional = emailInstitucional;
+        super(nome, emailInstitucional);
     }
 }
