d807e5de114d4c97881ee8eaa62809b2e4d12840
bee <becastroribeirode@gmail.com>
2025-10-30 14:13:27 -0300

    feat: Service para criar documento pdf do formulario e util necessario para tal

---
+++ src/utils/utilForm.ts
@@ -0,0 +1,21 @@
+import { PdfService } from '../services/PDFService';
+import { Assistida } from '../models/assistida/Assistida';
+
+const assistida = new Assistida(
+  'Maria Silva',
+  34,
+  'Feminino',
+  'Maria',
+  'Rua X, 123',
+  'Ensino Superior',
+  'Católica',
+  'Brasileira',
+  'Urbana',
+  'Advogada',
+  'Nenhuma',
+  '123456',
+  true,
+);
+
+const pdfService = new PdfService();
+pdfService.criarPdf(assistida);
