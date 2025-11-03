10e847df5ff76315174e4f6689dd3449e3ef0e65
Clidenor <clidenorlopesmartinsfilho@gmail.com>
2025-11-02 21:20:32 -0300

    feat(ControladorEncaminhamento): Cria controlador para orquestrar o registro de encaminhamentos

--- src/controllers/ControladorEncaminhamento.ts
+++ src/controllers/ControladorEncaminhamento.ts
@@ -4,9 +4,11 @@ import { OrgaoRedeApoio } from "../models/Rede-Apoio/OrgaoRedeApoio";
 import { Encaminhamento } from "../models/Rede-Apoio/Encaminhamento";
 import { Caso } from "../models/Caso/Caso";
 import { AssistenteSocial } from "../models/AssistenteSocial";
+import { Alteracao } from "../models/Caso/Alteracao";
 
 // Import de serviços:
 import { ServicoDeEmail } from "../services/ServicoDeEmail";
+//falta serviço de gerarPDF
 
 export class ControladorEncaminhamento {
     private servicoDeEmail: ServicoDeEmail;
@@ -18,9 +20,24 @@ export class ControladorEncaminhamento {
     public async registrarEncaminhamento(caso: Caso, orgao: OrgaoRedeApoio, motivo: string, observacoes: string, assistenteSocial: AssistenteSocial) {
         try {
             const encaminhamento = new Encaminhamento(new Date(), motivo, observacoes, orgao, assistenteSocial, caso);
-            try{
-                const infoEmail = await this.servicoDeEmail.enviarEmailAutomatico(encaminhamento);
+            const infoEmail = await this.servicoDeEmail.enviarEmailAutomatico(encaminhamento);
+
+            const tipo = "Encaminhamento";
+            const descricao = `Encaminhamento para o órgão ${orgao.getNome()} realizado com sucesso. Email enviado com ID: ${infoEmail.messageId}`;
+            const data = new Date();
+            const responsavel = assistenteSocial.getName();
+
+            const novoRegistroHistorico = new Alteracao(tipo, descricao, data, responsavel);
+
+            caso.getHistorico().adicionarAlteracao(novoRegistroHistorico);
+            caso.adicionarEncaminhamento(encaminhamento);
+
+            //Adicionar logica para salvar no banco de dados na próxima etapa NÃO ESQUECER DISSO
+            return encaminhamento;
+            } catch(error){
+                console.error("Controlador: Falha ao enviar email automático: ", error);
+
+                throw new Error("Falha ao enviar o e-mail. O encaminhamento não foi salvo.");
             }
-        }
-    }   
-}
+        } 
+    }
